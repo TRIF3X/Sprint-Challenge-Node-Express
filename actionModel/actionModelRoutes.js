4d895efae72af371f493e391d6c77bc00c2d9304
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-09 11:04:46 -0600

    actions complete

--- actionModel/actionModelRoutes.js
+++ actionModel/actionModelRoutes.js
@@ -13,5 +13,50 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const action = req.body
+    if(!req.body.project_id || !req.body.description || !req.body.notes) {
+        res.status(400).json({ message: 'Please provide a project id, a description, and notes' })
+    } else {
+       actDb.insert(action)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(err => {
+            res.response(500).json({ message: 'error adding project to db' })
+        })
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    if(!req.body.project_id || !req.body.description || !req.body.notes) {
+        res.status(400).json({ message: 'Please provide a project id, a description, and notes' })
+    } else {
+        actDb.update(id, req.body) 
+            .then(project => {
+                res.status(200).json({ message: 'project has been updated', project })
+            })
+            .catch(err => {
+                res.status(500).json({ message: 'Error updating project in database' })
+            })
+    }  
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+        actDb.remove(id)
+        .then(project => {
+            if(project) {
+                res.status(200).json({ message: `deleted project with id:${req.params.id}` })
+            } else {
+                res.status(404).json({ message: `project with id:${req.params.id} does not exist` })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error deleting project from database' })
+        })
+})
+
 
 module.exports = router;
